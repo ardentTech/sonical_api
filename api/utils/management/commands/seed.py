176d39b757a876350fa55b206fec8d47b628c858
Jonathan D. Baker <jbaker@comverge.com>
2016-11-07 14:47:24 -0700

    manufacturers seed use json

--- api/utils/management/commands/seed.py
+++ api/utils/management/commands/seed.py
@@ -5,8 +5,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from drivers.factories import DriverFactory
-from manufacturing.factories import ManufacturerFactory, MaterialFactory
-from manufacturing.seed_data import MANUFACTURERS, MATERIALS
+from manufacturing.factories import ManufacturerFactory
 
 
 class Command(BaseCommand):
@@ -18,7 +17,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self._create_manufacturers()
-        self._create_materials()
+#        self._create_materials()
         self._create_drivers()
         print("ALL DONE!")
 
@@ -46,14 +45,23 @@ class Command(BaseCommand):
         print("created {0} drivers".format(len(drivers)))
 
     def _create_manufacturers(self):
-        for m in MANUFACTURERS:
-            manufacturer = ManufacturerFactory.create(name=m[0], website=m[1])
+        manufacturers = []
+        path = os.path.join(
+            settings.BASE_DIR, "manufacturing", "seed", "manufacturers.json")
+
+        with open(path) as data:
+            manufacturers = json.load(data)
+
+        generator = (manufacturer["fields"] for manufacturer in manufacturers)
+        for m in generator:
+            manufacturer = ManufacturerFactory.create(
+                name=m["name"], website=m["website"])
             self.manufacturers[manufacturer.id] = manufacturer
 
-        print("created {0} manufacturers".format(len(MANUFACTURERS)))
+        print("created {0} manufacturers".format(len(manufacturers)))
 
-    def _create_materials(self):
-        for m in MATERIALS:
-            self.materials[m[0]] = MaterialFactory.create(name=m[0])
-
-        print("created {0} materials".format(len(MATERIALS)))
+#    def _create_materials(self):
+#        for m in MATERIALS:
+#            self.materials[m[0]] = MaterialFactory.create(name=m[0])
+#
+#        print("created {0} materials".format(len(MATERIALS)))
