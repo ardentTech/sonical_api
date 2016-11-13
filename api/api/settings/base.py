95d2b959194c496d9039568f53ff2de00130e3b8
Jonathan D. Baker <jbaker@comverge.com>
2016-11-13 12:13:31 -0700

    configure cors

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party
+    "corsheaders",
     "rest_framework",
     # app
     "drivers",
@@ -59,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
