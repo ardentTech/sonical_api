d7b21c7ba6fc18791a849fa94a56a74751c213db
Jonathan D. Baker <jbaker@comverge.com>
2016-12-19 14:38:35 -0700

    more cleanup+organizing

--- api/drivers/scrapers/parts_express.py
+++ api/drivers/scrapers/parts_express.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 import re
 
+from django.conf import settings
+
 from .scraper import Scraper
 
 
@@ -9,30 +11,33 @@ from .scraper import Scraper
 # @todo adjust XPATH patterns to match by text instead of just position indexes
 
 
-DEV_MODE = "dev"
-PRO_MODE = "pro"
-URL = "https://www.parts-express.com"
-XPATH_PATTERNS = {
-    "DC_RESISTANCE": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[2]/span[2]/text()',
-    "DRIVER_CATEGORY_XPATH": '//a[@id="lbCategoryName"]/@href',
-    "DRIVER_DETAIL_XPATH": '//a[@id="GridViewProdLink"]/@href',
-    "ELECTROMAGNETIC_Q": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[7]/span[2]/text()',
-    "MANUFACTURER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[6]/ul/li[1]/span[2]/text()',
-    "MAX_POWER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[4]/span[2]/text()',
-    "MECHANICAL_Q": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[5]/span[2]/text()',
-    "MODEL": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[6]/ul/li[2]/span[2]/text()',
-    "NEXT_PAGE_XPATH": '//a[@id="ctl00_ctl00_MainContent_uxEBCategory_uxEBProductList_uxBottomPagingLinks_aNextNav"]/@href',
-    "NOMINAL_DIAMETER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[1]/span[2]/text()',
-    "NOMINAL_IMPEDANCE": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[5]/span[2]/text()',
-    "RESONANT_FREQUENCY": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[1]/span[2]/text()',
-    "REVIEW_COUNT": '//*[@id="TurnToTopSummary"]/div[1]/div/div[2]/a[1]/text()',
-    "RMS_POWER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[2]/span[2]/text()',
-    "SENSITIVITY": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[8]/span[2]/text()',
-    "VOICE_COIL_INDUCTANCE": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[4]/span[2]/text()',
-}
+class BasePartsExpressScraper(Scraper):
+
+    URL = "https://www.parts-express.com"
+    XPATH = {
+        "DC_RESISTANCE": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[2]/span[2]/text()',
+        "DRIVER_CATEGORY": '//a[@id="lbCategoryName"]/@href',
+        "DRIVER_DETAIL": '//a[@id="GridViewProdLink"]/@href',
+        "ELECTROMAGNETIC_Q": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[7]/span[2]/text()',
+        "MANUFACTURER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[6]/ul/li[1]/span[2]/text()',
+        "MAX_POWER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[4]/span[2]/text()',
+        "MECHANICAL_Q": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[5]/span[2]/text()',
+        "MODEL": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[6]/ul/li[2]/span[2]/text()',
+        "NEXT_PAGE": '//a[@id="ctl00_ctl00_MainContent_uxEBCategory_uxEBProductList_uxBottomPagingLinks_aNextNav"]/@href',
+        "NOMINAL_DIAMETER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[1]/span[2]/text()',
+        "NOMINAL_IMPEDANCE": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[5]/span[2]/text()',
+        "RESONANT_FREQUENCY": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[1]/span[2]/text()',
+        "REVIEW_COUNT": '//*[@id="TurnToTopSummary"]/div[1]/div/div[2]/a[1]/text()',
+        "RMS_POWER": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[2]/span[2]/text()',
+        "SENSITIVITY": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[1]/ul/li[8]/span[2]/text()',
+        "VOICE_COIL_INDUCTANCE": '//*[@id="ctl00_ctl00_MainContent_uxProduct_pnlProductDetails"]/div[2]/div[3]/div[2]/ul/li[4]/span[2]/text()',
+    }
+
+    def url_from_path(self, path):
+        return self.URL + path
 
 
-class DriverScraper(Scraper):
+class DriverScraper(BasePartsExpressScraper):
 
     def __init__(self, mode):
         _attrs = self._get_driver_attrs()
@@ -42,7 +47,7 @@ class DriverScraper(Scraper):
         self.data = {}
 
     def run(self, path):
-        self.data["data_source"] = URL + path
+        self.data["data_source"] = self.url_from_path(path)
         tree = self._get_tree(self.data["data_source"])
 
         for attr in self.driver_attrs:
@@ -55,18 +60,18 @@ class DriverScraper(Scraper):
 
     def _get_driver_attrs(self):
         return [
-            ("dc_resistance", XPATH_PATTERNS["DC_RESISTANCE"], self._to_decimal),
-            ("electromagnetic_q", XPATH_PATTERNS["ELECTROMAGNETIC_Q"], None),
-            ("manufacturer", XPATH_PATTERNS["MANUFACTURER"], None),
-            ("max_power", XPATH_PATTERNS["MAX_POWER"], self._to_int),
-            ("mechanical_q", XPATH_PATTERNS["MECHANICAL_Q"], None),
-            ("model", XPATH_PATTERNS["MODEL"], None),
-            ("nominal_diameter", XPATH_PATTERNS["NOMINAL_DIAMETER"], None),
-            ("nominal_impedance", XPATH_PATTERNS["NOMINAL_IMPEDANCE"], self._to_decimal),
-            ("resonant_frequency", XPATH_PATTERNS["RESONANT_FREQUENCY"], self._to_decimal),
-            ("rms_power", XPATH_PATTERNS["RMS_POWER"], self._to_int),
-            ("sensitivity", XPATH_PATTERNS["SENSITIVITY"], self._to_decimal),
-            ("voice_coil_inductance", XPATH_PATTERNS["VOICE_COIL_INDUCTANCE"], self._to_decimal)]
+            ("dc_resistance", self.XPATH["DC_RESISTANCE"], self._to_decimal),
+            ("electromagnetic_q", self.XPATH["ELECTROMAGNETIC_Q"], None),
+            ("manufacturer", self.XPATH["MANUFACTURER"], None),
+            ("max_power", self.XPATH["MAX_POWER"], self._to_int),
+            ("mechanical_q", self.XPATH["MECHANICAL_Q"], None),
+            ("model", self.XPATH["MODEL"], None),
+            ("nominal_diameter", self.XPATH["NOMINAL_DIAMETER"], None),
+            ("nominal_impedance", self.XPATH["NOMINAL_IMPEDANCE"], self._to_decimal),
+            ("resonant_frequency", self.XPATH["RESONANT_FREQUENCY"], self._to_decimal),
+            ("rms_power", self.XPATH["RMS_POWER"], self._to_int),
+            ("sensitivity", self.XPATH["SENSITIVITY"], self._to_decimal),
+            ("voice_coil_inductance", self.XPATH["VOICE_COIL_INDUCTANCE"], self._to_decimal)]
 
     def _to_decimal(self, val):
         return Decimal(val.split(" ")[0])
@@ -75,56 +80,56 @@ class DriverScraper(Scraper):
         return int(val.split(" ")[0])
 
 
-class DriverListScraper(Scraper):
+class DriverListScraper(BasePartsExpressScraper):
 
     def __init__(self, mode):
         self.data = []
-        self.should_paginate = False if mode == DEV_MODE else True
+        self.should_paginate = False if mode == self.DEV_MODE else True
 
     def run(self, path):
-        tree = self._get_tree(URL + path)
+        tree = self._get_tree(self.url_from_path(path))
 
-        for driver in tree.xpath(XPATH_PATTERNS["DRIVER_DETAIL_XPATH"]):
+        for driver in tree.xpath(self.XPATH["DRIVER_DETAIL"]):
             self.data.append(driver)
 
         if self.should_paginate:
             try:
-                return self.run(tree.xpath(XPATH_PATTERNS["NEXT_PAGE_XPATH"])[0])
+                return self.run(tree.xpath(self.XPATH["NEXT_PAGE"])[0])
             except IndexError:
                 return self.data
 
         return self.data
 
 
-class CategoryListScraper(Scraper):
+class CategoryListScraper(BasePartsExpressScraper):
 
     def __init__(self, mode):
         self.data = []
         self.mode = mode
 
     def run(self, path):
-        categories = self._get_tree(URL + path).xpath(XPATH_PATTERNS["DRIVER_CATEGORY_XPATH"])
+        categories = self._get_tree(
+            self.url_from_path(path)).xpath(
+                self.XPATH["DRIVER_CATEGORY"])
         self.data = [c for c in categories if not re.search(r'replace|recone', c)]
 
-        if self.mode == DEV_MODE:
+        if self.mode == self.DEV_MODE:
             self.data = self.data[:1]
 
         return self.data
 
 
-class PartsExpressScraper(Scraper):
+class PartsExpressScraper(BasePartsExpressScraper):
 
     INITIAL_CATEGORY_PATH = "/cat/hi-fi-woofers-subwoofers-midranges-tweeters/13"
 
     def __init__(self):
-        # @todo this should be an arg later
-        self.mode = DEV_MODE
+        self.data = []
+        self.mode = self.DEV_MODE if settings.DEBUG is True else self.PRO_MODE
 
     def run(self):
-        drivers = []
-
         for cp in CategoryListScraper(mode=self.mode).run(self.INITIAL_CATEGORY_PATH):
             for dp in DriverListScraper(mode=self.mode).run(cp):
-                drivers.append(DriverScraper(mode=self.mode).run(dp))
+                self.data.append(DriverScraper(mode=self.mode).run(dp))
 
-        return drivers
+        return self.data
--- api/drivers/scrapers/scraper.py
+++ api/drivers/scrapers/scraper.py
@@ -10,12 +10,15 @@ SCRAPER_ID = "Sonical Scraper 1.0 (jonathan@ardent.tech)"
 # @todo report unprocessed/processed totals
 class Scraper(object):
 
+    DEV_MODE = "dev"
+    PRO_MODE = "pro"
+
     def run(self, path):
         raise Exception("Sub-classes of Scraper must implement `run(self, path)`")
 
     def _get_tree(self, url):
+        sleep(1)
         headers = {"user-agent": SCRAPER_ID}
         page = requests.get(url, headers=headers)
         tree = html.fromstring(page.content)
-        sleep(1)
         return tree
