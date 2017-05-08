68dd525a034a3f3ed0ab2ca5a6f45d1eb6931493
Jonathan D. Baker <jbaker@comverge.com>
2017-05-07 21:07:48 -0600

    expand driver filtering options

--- api/drivers/filters.py
+++ api/drivers/filters.py
@@ -4,26 +4,25 @@ from .models import Driver
 from utils.filter_fields import SonicalBooleanFilter
 
 
-# @todo allow gt lt gte lte in addition to equality
+# @todo re-add custom in_production filter
+# @todo research 'icontains' for char fields
 class DriverFilter(django_filters.FilterSet):
 
     in_production = SonicalBooleanFilter()
-#    manufacturer = django_filters.CharFilter(name="manufacturer__name")
 
     class Meta:
-        fields = [
-            "dc_resistance",
-            "electromagnetic_q",
-            "in_production",
-            "manufacturer",
-            "max_power",
-            "mechanical_q",
-            "model",
-            "nominal_diameter",
-            "nominal_impedance",
-            "resonant_frequency",
-            "rms_power",
-            "sensitivity",
-            "voice_coil_inductance",
-        ]
+        fields = {
+            "dc_resistance": ["exact", "gt", "gte", "lt", "lte"],
+            "electromagnetic_q": ["exact", "gt", "gte", "lt", "lte"],
+            "manufacturer": ["exact"],
+            "max_power": ["exact", "gt", "gte", "lt", "lte"],
+            "mechanical_q": ["exact", "gt", "gte", "lt", "lte"],
+            "model": ["contains"],
+            "nominal_diameter": ["exact", "gt", "gte", "lt", "lte"],
+            "nominal_impedance": ["exact", "gt", "gte", "lt", "lte"],
+            "resonant_frequency": ["exact", "gt", "gte", "lt", "lte"],
+            "rms_power": ["exact", "gt", "gte", "lt", "lte"],
+            "sensitivity": ["exact", "gt", "gte", "lt", "lte"],
+            "voice_coil_inductance": ["exact", "gt", "gte", "lt", "lte"],
+        }
         model = Driver
